33851033887f8e6ef5b04524ea40110d88a02a9d
Ranflin Diaz <54969258+ranflindiaz@users.noreply.github.com>
2022-11-26 17:04:17 -0500

    Creating EndPoints

---
+++ typings.d.ts
@@ -0,0 +1,69 @@
+import Image from "next/image";
+
+interface SanityBody {
+    _createdAt: string;
+    _id: string;
+    _rev: string;
+    _updateAt: string;
+}
+
+interface Image {
+    _type: 'image';
+    asset: {
+        _ref: string;
+        _type: 'reference';
+    };
+}
+
+export interface PageInfo extends SanityBody {
+    _type: 'pageInfo';
+    address: string;
+    BackgroundInformation: string;
+    email: string;
+    role: string;
+    heroImage: Image;
+    name: string;
+    phoneNumber: string;
+    profilePic: Image
+}
+
+export interface Technology extends SanityBody {
+    _type: 'skill';
+    image: Image;
+    progress: number;
+    title: string;
+}
+
+export interface Skill extends SanityBody {
+    _type: 'skill';
+    image: Image;
+    progress: number;
+    title: string;
+}
+
+export interface Experience extends SanityBody {
+    _type: 'experience';
+    company: string;
+    companyImage: Image;
+    dateStarted: date;
+    dateEnded: date;    
+    isCurrentlyWorkingHere: boolean;
+    joTitle: string;
+    points: string[];
+    technologies: Technology[];
+}
+
+export interface Project extends SanityBody {
+    title: string;
+    _type: 'skill';
+    image: Image;
+    linkToBuild: string;
+    summary: string;
+    technologies: Technology[];    
+}
+
+export interface Social extends SanityBody {
+    _type: 'social';
+    title: string;
+    url: string;
+}
---
+++ utils/fetchSkills.ts
@@ -0,0 +1,1 @@
+import { Skill } from '../typings'
